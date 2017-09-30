7ccf7a0bd12f220abe58ad905e3fc41b41f01243
jeanhindy <31793348+jeanhindy@users.noreply.github.com>
2017-09-30 12:27:05 +0300

    Update loragw_spi.h

--- lora_gateway/libloragw/inc/loragw_spi.h
+++ lora_gateway/libloragw/inc/loragw_spi.h
@@ -6,11 +6,11 @@
 (______/|_____)_|_|_| \__)_____)\____)_| |_|
   (C)2013 Semtech-Cycleo
 Description:
-    Host specific functions to address the LoRa concentrator registers through a
-    SPI interface.
-    Single-byte read/write and burst read/write.
-    Does not handle pagination.
-    Could be used with multiple SPI ports in parallel (explicit file descriptor)
+	Host specific functions to address the LoRa concentrator registers through a
+	SPI interface.
+	Single-byte read/write and burst read/write.
+	Does not handle pagination.
+	Could be used with multiple SPI ports in parallel (explicit file descriptor)
 License: Revised BSD License, see LICENSE.TXT file include in the project
 Maintainer: Sylvain Miermont
 */
@@ -22,24 +22,16 @@ Maintainer: Sylvain Miermont
 /* -------------------------------------------------------------------------- */
 /* --- DEPENDANCIES --------------------------------------------------------- */
 
-#include <stdint.h>        /* C99 types*/
+#include <stdint.h>		/* C99 types*/
 
-#include "config.h"    /* library configuration options (dynamically generated) */
+#include "config.h"	/* library configuration options (dynamically generated) */
 
 /* -------------------------------------------------------------------------- */
 /* --- PUBLIC CONSTANTS ----------------------------------------------------- */
 
-#define LGW_SPI_SUCCESS     0
-#define LGW_SPI_ERROR       -1
-#define LGW_BURST_CHUNK     1024
-
-#define LGW_SPI_MUX_MODE0   0x0     /* No FPGA */
-#define LGW_SPI_MUX_MODE1   0x1     /* FPGA, with spi mux header */
-
-#define LGW_SPI_MUX_TARGET_SX1301   0x0
-#define LGW_SPI_MUX_TARGET_FPGA     0x1
-#define LGW_SPI_MUX_TARGET_EEPROM   0x2
-#define LGW_SPI_MUX_TARGET_SX127X   0x3
+#define LGW_SPI_SUCCESS	 0
+#define LGW_SPI_ERROR	-1
+#define LGW_BURST_CHUNK	 1024
 
 /* -------------------------------------------------------------------------- */
 /* --- PUBLIC FUNCTIONS PROTOTYPES ------------------------------------------ */
@@ -67,7 +59,7 @@ int lgw_spi_close(void *spi_target);
 @param data data byte to write
 @return status of register operation (LGW_SPI_SUCCESS/LGW_SPI_ERROR)
 */
-int lgw_spi_w(void *spi_target, uint8_t spi_mux_mode, uint8_t spi_mux_target, uint8_t address, uint8_t data);
+int lgw_spi_w(void *spi_target, uint8_t address, uint8_t data);
 
 /**
 @brief LoRa concentrator SPI single-byte read
@@ -76,7 +68,7 @@ int lgw_spi_w(void *spi_target, uint8_t spi_mux_mode, uint8_t spi_mux_target, ui
 @param data data byte to write
 @return status of register operation (LGW_SPI_SUCCESS/LGW_SPI_ERROR)
 */
-int lgw_spi_r(void *spi_target, uint8_t spi_mux_mode, uint8_t spi_mux_target, uint8_t address, uint8_t *data);
+int lgw_spi_r(void *spi_target, uint8_t address, uint8_t *data);
 
 /**
 @brief LoRa concentrator SPI burst (multiple-byte) write
@@ -86,7 +78,7 @@ int lgw_spi_r(void *spi_target, uint8_t spi_mux_mode, uint8_t spi_mux_target, ui
 @param size size of the transfer, in byte(s)
 @return status of register operation (LGW_SPI_SUCCESS/LGW_SPI_ERROR)
 */
-int lgw_spi_wb(void *spi_target, uint8_t spi_mux_mode, uint8_t spi_mux_target, uint8_t address, uint8_t *data, uint16_t size);
+int lgw_spi_wb(void *spi_target, uint8_t address, uint8_t *data, uint16_t size);
 
 /**
 @brief LoRa concentrator SPI burst (multiple-byte) read
@@ -96,6 +88,8 @@ int lgw_spi_wb(void *spi_target, uint8_t spi_mux_mode, uint8_t spi_mux_target, u
 @param size size of the transfer, in byte(s)
 @return status of register operation (LGW_SPI_SUCCESS/LGW_SPI_ERROR)
 */
-int lgw_spi_rb(void *spi_target, uint8_t spi_mux_mode, uint8_t spi_mux_target, uint8_t address, uint8_t *data, uint16_t size);
+int lgw_spi_rb(void *spi_target, uint8_t address, uint8_t *data, uint16_t size);
 
 #endif
+
+/* --- EOF ------------------------------------------------------------------ */
